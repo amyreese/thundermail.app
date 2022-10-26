1b31628c27fce8cb27bb53d500440ad209c36bff
Jason Weathered <jason@jasoncodes.com>
2022-10-27 08:05:01 +1000

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eou pipefail
+
+cd "$(dirname "$0")"
+set -x
+
+xcodebuild
+rm -rf ~/Library/Mail/Bundles/GMailinator.mailbundle
+cp -a /tmp/GMailinator/GMailinator.mailbundle ~/Library/Mail/Bundles/
+spctl --add ~/Library/Mail/Bundles/GMailinator.mailbundle
+defaults write com.apple.mail EnableBundles -bool true
