23c80f4e588033ba227bacf01ce7ff88ee9df329
Amethyst Reese <amethyst@n7.gg>
2023-08-24 18:24:05 -0700

    Improve install script

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ set -x
 
 xcodebuild
 rm -rf ~/Library/Mail/Bundles/GMailinator.mailbundle
-cp -a /tmp/GMailinator/GMailinator.mailbundle ~/Library/Mail/Bundles/
+cp -a build/Release/GMailinator.mailbundle ~/Library/Mail/Bundles/
 spctl --add ~/Library/Mail/Bundles/GMailinator.mailbundle
 defaults write com.apple.mail EnableBundles -bool true
